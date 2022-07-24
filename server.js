5cf35733e977deafb65e78a99515c8b07cdeccc9
Timur Khayretdinov <timur3063@yandex.ru>
2022-07-24 13:16:40 +0300

    deploy fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const server = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const shared = path.join(__dirname, "/dist");
 
